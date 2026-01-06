235cc1c58d3d557a0ddad1fb9cb5c579220a15ec
harrisonnordstrom981 <harrisonnordstrom2@gmail.com>
2026-01-06 15:35:15 -0500

    Update 04-kpi-focus.sql

--- sql/04-kpi-focus.sql
+++ sql/04-kpi-focus.sql
@@ -1,13 +1,10 @@
--- 04_part_b_kpis.sql
--- Purpose: KPI summary (2023)
+-- 04-kpi-focus.sql
+-- KPI summary for latest year (2023)
 
 CREATE OR REPLACE VIEW `project-3-medicare.medicare_data.vw_part_b_kpis_2023` AS
 SELECT
-  2023 AS year,
-  SUM(tot_spndng) AS total_spending,
-  SUM(tot_clms) AS total_claims,
-  SUM(tot_benes) AS total_beneficiaries,
-  AVG(avg_spnd_per_unit) AS avg_spending_per_unit,
-  AVG(avg_spnd_per_bene) AS avg_spending_per_beneficiary
+  SUM(total_spending) AS total_spending_2023,
+  COUNT(DISTINCT hcpcs_cd) AS unique_drugs_2023,
+  AVG(avg_spending_per_unit) AS avg_price_per_unit_2023
 FROM `project-3-medicare.medicare_data.vw_part_b_long`
 WHERE year = 2023;
