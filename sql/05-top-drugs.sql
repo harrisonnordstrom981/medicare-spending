f2136919224f78e3475bc53429eaa3b400ec6a6f
harrisonnordstrom981 <harrisonnordstrom2@gmail.com>
2026-01-06 15:35:25 -0500

    Update 05-top-drugs.sql

--- sql/05-top-drugs.sql
+++ sql/05-top-drugs.sql
@@ -1,15 +1,12 @@
--- 05_part_b_top_drugs.sql
--- Purpose: Rank drugs by total spending (2023)
+-- 05-top-drugs.sql
+-- Top Medicare Part B drugs by spending (2023)
 
 CREATE OR REPLACE VIEW `project-3-medicare.medicare_data.vw_part_b_top_spending_2023` AS
 SELECT
   hcpcs_cd,
-  COALESCE(NULLIF(brand_name, ''), NULLIF(generic_name, ''), hcpcs_desc) AS drug_name,
-  tot_spndng,
-  tot_benes,
-  tot_clms,
-  avg_spnd_per_unit,
-  outlier_flag
+  drug_name,
+  total_spending
 FROM `project-3-medicare.medicare_data.vw_part_b_long`
 WHERE year = 2023
-ORDER BY tot_spndng DESC;
+ORDER BY total_spending DESC
+LIMIT 20;
